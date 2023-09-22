ecb7a5b24fa69a4e4c1925c0078abeebc7ac5d42
FA20-BSE-055 <101318433+FA20-BSE-055@users.noreply.github.com>
2023-09-22 10:08:41 +0500

    singleton

---
+++ labs/lab 2/Singleton/src/MySingletonn.java
@@ -0,0 +1,32 @@
+
+
+class MySingleton
+{
+	static MySingleton instance = null;
+	public int x = 10;
+	
+	
+	private MySingleton() { }
+
+	
+	static public MySingleton getInstance()
+	{
+		if (instance == null)		
+			instance = new MySingleton();
+
+		return instance;
+	}
+}
+
+
+class Main
+{
+public static void main(String args[])	
+{
+	MySingleton a = MySingleton.getInstance();
+	MySingleton b = MySingleton.getInstance();
+	a.x = a.x + 10;
+	System.out.println("Value of a.x = " + a.x);
+	System.out.println("Value of b.x = " + b.x);
+}	
+}
