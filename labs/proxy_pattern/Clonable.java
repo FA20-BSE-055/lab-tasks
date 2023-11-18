3e3c53fd6b8bea5debd580085d25fcdf38dffbcc
FA20-BSE-055 <101318433+FA20-BSE-055@users.noreply.github.com>
2023-11-18 03:29:25 -0800

    Update Clonable.java

--- labs/proxy_pattern/Clonable.java
+++ labs/proxy_pattern/Clonable.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package proxy_pattern;
 
-/**
- *
- * @author PAKISTAN
- */
+
 public interface Clonable {
     Clonable clone();
 }
