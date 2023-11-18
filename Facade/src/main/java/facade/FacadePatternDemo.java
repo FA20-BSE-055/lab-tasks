078c2fae8863ee7258ae7ade9420cb30e9cef43b
FA20-BSE-055 <101318433+FA20-BSE-055@users.noreply.github.com>
2023-11-18 02:34:36 -0800

    facade_pattern

---
+++ Facade/src/main/java/facade/FacadePatternDemo.java
@@ -0,0 +1,16 @@
+
+package facade;
+
+public class FacadePatternDemo {
+    public static void main(String[] args) {
+      ShapeMaker shapeMaker = new ShapeMaker();
+
+      shapeMaker.drawCircle();
+      shapeMaker.drawRectangle();
+      shapeMaker.drawSquare();	
+      shapeMaker.drawTriangle();
+      shapeMaker.drawOval();
+
+   }
+}
+
